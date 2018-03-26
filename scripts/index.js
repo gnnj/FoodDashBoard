0cbf0209fa6f95eaa8f1c96ae700e875452f7254
Timothy Yu <tmichael.yu@gmail.com>
2018-03-25 20:47:05 -0400

    dropdown not working for dypgraph route; route not implemented

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,18 @@
 //Javascript for index page
+
+
+// Create dropdown from Flask
+d3.json("/states", function(error, response) {
+
+    if (error) return console.warn(error);
+
+    // console.log(response);
+
+    var $dropDown = document.getElementById("selDataset")
+
+    for (var i=0; i< response.length; i++){
+        var $optionChoice = document.createElement("option");
+        $optionChoice.innerHTML = response[i];
+        $optionChoice.setAttribute("value", response[i]);
+        $dropDown.appendChild($optionChoice);
+    }
